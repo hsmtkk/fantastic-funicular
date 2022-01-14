d0e379991e1d925336693c8d297e4c91bbb33327
hsmtkk@gmail.com <hsmtkk@gmail.com>
2022-01-15 07:40:21 +0900

    works well

---
+++ build-linux.ps1
@@ -0,0 +1,3 @@
+#rustup target add x86_64-unknown-linux-musl
+cargo build --release --target=x86_64-unknown-linux-musl
+cp target/x86_64-unknown-linux-musl/release/fantastic-funicular fantastic-funicular.bin
